55765eb660337ddcef399a433b2a26f08a5079f4
João Vitor <joaovnevescosta@hotmail.com>
2024-06-18 11:35:14 -0300

    Hedgehog commit

--- app/src/main/java/br/com/jvn/estacionajuntos/view/MainActivity.java
+++ app/src/main/java/br/com/jvn/estacionajuntos/view/MainActivity.java
@@ -14,19 +14,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import br.com.jvn.estacionajuntos.R;
-//aa
+
 public class MainActivity extends AppCompatActivity {
     Button btnCadastrar, btnEntrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
         //TODO: Find what is data_extraction_rules.xml
         setLayout();
