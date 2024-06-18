662117f1b958761ee7ca8e234299fe0e89cb0153
João Vitor <joaovnevescosta@hotmail.com>
2024-06-18 11:29:50 -0300

    initial commit

--- app/src/main/java/br/com/jvn/estacionajuntos/view/MainActivity.java
+++ app/src/main/java/br/com/jvn/estacionajuntos/view/MainActivity.java
@@ -14,7 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import br.com.jvn.estacionajuntos.R;
-
+//aa
 public class MainActivity extends AppCompatActivity {
     Button btnCadastrar, btnEntrar;
     @Override
